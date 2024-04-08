afed17c68015591661822979df283fc8af29e022
rodrigosdmenezes <rodrigosdmenezes@gmail.com>
2024-04-08 11:10:39 -0300

    Criando tabelas

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CoursesRoutingModule } from './courses-routing.module';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { AppMaterialModule } from '../../shared/app-material/app-material.module';
+
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [AppMaterialModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
+
 export class CoursesComponent {
+
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'Front End'}
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor(){
+    //this.courses = [];
+  }
+
+  ngOnInit():void{
+    
+  }
 
 }

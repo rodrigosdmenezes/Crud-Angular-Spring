d45372acb52d5ed248f49e601a6379ee5f4ef05b
rodrigosdmenezes <rodrigosdmenezes@gmail.com>
2024-04-10 10:07:37 -0300

    Conectando Front-End com Back-End

--- Crud-Angular/src/app/courses/services/courses.service.ts
+++ Crud-Angular/src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) { }
 

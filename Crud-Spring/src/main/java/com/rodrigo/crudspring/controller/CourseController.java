fc8b082a887ae38a87950f4a5b7d404f021de788
rodrigosdmenezes <rodrigosdmenezes@gmail.com>
2024-04-11 09:12:54 -0300

    Criando endpoint para adicionar cursos

--- Crud-Spring/src/main/java/com/rodrigo/crudspring/controller/CourseController.java
+++ Crud-Spring/src/main/java/com/rodrigo/crudspring/controller/CourseController.java
@@ -2,9 +2,14 @@ package com.rodrigo.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodrigo.crudspring.model.Course;
@@ -22,6 +27,12 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+        return courseRepository.save(course);
     }
 
 }
